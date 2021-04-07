db6fda867144f9099061d3901b7432e66cf4b0e8
LyubomirKuzov <lyubomir.kuzov.99@gmail.com>
2021-04-07 20:13:29 +0300

    Add

---
+++ 15. Regular Expressions/MatchFullName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            string names = Console.ReadLine();
+
+            var matches = Regex.Matches(names, pattern);
+
+            foreach (Match name in matches)
+            {
+                Console.Write(name.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
